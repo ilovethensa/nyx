1668e625f3b7c47e84cdbf10d5c3c533e6c3219c
TheHolyTachanka <me@theholytachanka.com>
2025-03-18 14:35:36 +0200

    fixed warnings

--- hosts/viper/configuration.nix
+++ hosts/viper/configuration.nix
@@ -23,7 +23,7 @@
 
   time.timeZone = "Europe/Sofia";
 
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   # rtkit is optional but recommended
   security.rtkit.enable = true;
   services.pipewire = {
