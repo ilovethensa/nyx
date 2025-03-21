e82315077bcc7c413184dbe30da34200b4282e48
TheHolyTachanka <me@theholytachanka.com>
2025-03-21 10:50:44 +0200

    fuck bots

--- hosts/ikaros/services/caddy.nix
+++ hosts/ikaros/services/caddy.nix
@@ -2,54 +2,106 @@
   services.caddy = {
     enable = true;
     virtualHosts = {
-      #"theholytachanka.com".extraConfig = ''
-      #  encode gzip
-      #  reverse_proxy http://localhost:5463
-      #'';
-      "pwned.page".extraConfig = ''
-        encode gzip
-        file_server
-        root * /var/www/pwned.page/
-      '';
       "automate.theholytachanka.com".extraConfig = ''
         encode gzip
         reverse_proxy http://localhost:5678
-      '';
-      "smarthome.theholytachanka.com".extraConfig = ''
-        encode gzip
-        reverse_proxy http://localhost:8123
-      '';
-      "request.theholytachanka.com".extraConfig = ''
-        encode gzip
-        reverse_proxy http://localhost:5055
-      '';
-      "lr.pwned.page".extraConfig = ''
-        encode gzip
-        reverse_proxy http://localhost:8080
-      '';
-      "rimgo.pwned.page".extraConfig = ''
-        encode gzip
-        reverse_proxy http://localhost:3000
-      '';
-      "watch.theholytachanka.com".extraConfig = ''
-        encode gzip
-        reverse_proxy http://localhost:8096
-      '';
-      "yt.pwned.page".extraConfig = ''
-        encode gzip
-        reverse_proxy http://localhost:1234
-      '';
-      "notes.theholytachanka.com".extraConfig = ''
-        encode gzip
-        reverse_proxy http://localhost:6806
       '';
       "git.theholytachanka.com".extraConfig = ''
         encode gzip
         reverse_proxy http://localhost:3333
       '';
+      "notes.theholytachanka.com".extraConfig = ''
+        encode gzip
+        reverse_proxy http://localhost:6806
+      '';
+      "request.theholytachanka.com".extraConfig = ''
+        encode gzip
+        reverse_proxy http://localhost:5055
+      '';
       "rss.theholytachanka.com".extraConfig = ''
         encode gzip
         reverse_proxy http://localhost:8082
+      '';
+      "smarthome.theholytachanka.com".extraConfig = ''
+        encode gzip
+        reverse_proxy http://localhost:8123
+      '';
+      "watch.theholytachanka.com".extraConfig = ''
+        encode gzip
+        reverse_proxy http://localhost:8096
+      '';
+      "lr.pwned.page".extraConfig = ''
+        encode gzip
+        reverse_proxy http://localhost:8080
+
+        @robot {
+            header User-Agent *bot*
+            header User-Agent *spider*
+            header User-Agent *ai*
+            header_regexp ua (?i)(AdsBot-Google|Amazonbot|anthropic-ai|Applebot|Applebot-Extended|AwarioRssBot|AwarioSmartBot|Bytespider)
+            not path /robots.txt
+        }
+        handle @robot {
+            file_server {
+                root /mnt/data/Backup
+            }
+            rewrite * /capital.txt
+        }
+      '';
+      "pwned.page".extraConfig = ''
+        encode gzip
+        file_server
+        root * /var/www/pwned.page/
+
+        @robot {
+            header User-Agent *bot*
+            header User-Agent *spider*
+            header User-Agent *ai*
+            header_regexp ua (?i)(AdsBot-Google|Amazonbot|anthropic-ai|Applebot|Applebot-Extended|AwarioRssBot|AwarioSmartBot|Bytespider)
+            not path /robots.txt
+        }
+        handle @robot {
+            file_server {
+                root /mnt/data/Backup
+            }
+            rewrite * /capital.txt
+        }
+      '';
+      "rimgo.pwned.page".extraConfig = ''
+        encode gzip
+        reverse_proxy http://localhost:3000
+
+        @robot {
+            header User-Agent *bot*
+            header User-Agent *spider*
+            header User-Agent *ai*
+            header_regexp ua (?i)(AdsBot-Google|Amazonbot|anthropic-ai|Applebot|Applebot-Extended|AwarioRssBot|AwarioSmartBot|Bytespider)
+            not path /robots.txt
+        }
+        handle @robot {
+            file_server {
+                root /mnt/data/Backup
+            }
+            rewrite * /capital.txt
+        }
+      '';
+      "yt.pwned.page".extraConfig = ''
+        encode gzip
+        reverse_proxy http://localhost:1234
+
+        @robot {
+            header User-Agent *bot*
+            header User-Agent *spider*
+            header User-Agent *ai*
+            header_regexp ua (?i)(AdsBot-Google|Amazonbot|anthropic-ai|Applebot|Applebot-Extended|AwarioRssBot|AwarioSmartBot|Bytespider)
+            not path /robots.txt
+        }
+        handle @robot {
+            file_server {
+                root /mnt/data/Backup
+            }
+            rewrite * /capital.txt
+        }
       '';
     };
   };
