f00ba5b5bd4104484656dbfb72bbf2c28637a7dd
ilovethensa <145558184+ilovethensa@users.noreply.github.com>
2025-04-13 22:20:36 +0300

    Update configuration.nix

--- hosts/ikaros/configuration.nix
+++ hosts/ikaros/configuration.nix
@@ -43,8 +43,8 @@
   services.openssh = {
     enable = true;
     # require public key authentication for better security
-    settings.PasswordAuthentication = false;
-    settings.KbdInteractiveAuthentication = false;
+    settings.PasswordAuthentication = true;
+    settings.KbdInteractiveAuthentication = true;
     settings.PermitRootLogin = "yes";
   };
 
